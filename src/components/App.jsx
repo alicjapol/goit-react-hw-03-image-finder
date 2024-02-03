75174ba3d343f7ec8a50ab502cc2e0b0f9dcae43
alicjapol <alicjaxpoltorak@gmail.com>
2024-02-03 17:58:37 +0100

    modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -89,7 +89,7 @@ export function App() {
   };
 
   return (
-    <div>
+<div>
       <AppWrapper>
         <SearchbarWrapper>
           <Searchbar onSubmit={handleInputChange} />
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,28 @@
+import styled from 'styled-components';
+
+const Overlay = styled.div ` 
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 1200;
+`
+
+const ModalWindow =  styled.div `
+max-width: calc(100vw - 48px);
+max-height: calc(100vh - 24px);`
+
 export default function Modal({ imageURL, onClose }) {
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal">
+    <Overlay className="overlay" onClick={onClose}>
+      <ModalWindow className="modal">
         <img src={imageURL} alt="Full Size" />
-      </div>
-    </div>
+      </ModalWindow>
+    </Overlay>
   );
 }
